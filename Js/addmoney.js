49ef12150131fa2e01294102ef480906d1a785c1
Nazmul <nhmoonofficial@gmail.com>
2025-09-02 22:32:37 +0600

    Cash out folder added and link up to that page

--- Js/addmoney.js
+++ Js/addmoney.js
@@ -1,3 +1,8 @@
 document.getElementById('btnAddMoney').addEventListener('click', function(){
   alert('See you soon bro..!!')
 })
+
+
+document.getElementById('cashOut').addEventListener('click', function(){
+window.location.href = " ../Html/cashout.html"
+});
