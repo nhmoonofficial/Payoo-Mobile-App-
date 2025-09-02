4d5f05005f4fdb530866d6dc293294a04e6fe958
Nazmul <nhmoonofficial@gmail.com>
2025-09-03 01:43:27 +0600

    add pages are ready to go

--- Js/home.js
+++ Js/home.js
@@ -1,7 +1,27 @@
+document.getElementById('btnLogout').addEventListener('click',function(){
+  window.location.href = "../Html/login.html"
+})
+
 document.getElementById('addMoney').addEventListener('click', function(){
   window.location.href = '../Html/addmoney.html';
 })
+document.getElementById('cashOut').addEventListener('click', function(){
+  window.location.href = '../Html/cashout.html';
+})
+document.getElementById('transferMoney').addEventListener('click', function(){
+  window.location.href = '../Html/transfermoney.html';
+})
+document.getElementById('getBonus').addEventListener('click', function(){
+  window.location.href = '../Html/getbonus.html';
+})
+document.getElementById('payBill').addEventListener('click', function(){
+  window.location.href = '../Html/paybill.html';
+})
+document.getElementById('transaction').addEventListener('click', function(){
+  window.location.href = '../Html/transaction.html';
+})
 
-document.getElementById('homeLogout').addEventListener('click',function(){
-  window.location.href = "../Html/login.html"
+
+document.getElementById('viewBtnHome').addEventListener('click', function(){
+  alert('No More Transaction Found');
 })
